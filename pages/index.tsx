c693f9c963ce7012c75d5fee1b769347cc1d4bea
shenlj210104@stu.nfu.edu.cn <shenlj210104@stu.nfu.edu.cn>
2024-08-08 12:46:11 +0000

    调整样式

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,27 +32,33 @@ const Home: React.FC = () => {
     if (language === 'html') setHtml(value || '');
     if (language === 'css') setCss(value || '');
     if (language === 'javascript') {
-      setJs(value || '')
-      try {
-        const oldLog = console.log;
-        const oldError = console.error;
-
-        // 重定向 console.log 和 console.error
-        console.log = (message: any) => {
-          appendMessage(`${message}`, false);
-        };
-        console.error = (error: any) => {
-          appendMessage(`Error: ${error}`, true);
-        };
-
-        // 注意：使用 eval() 有一定的安全风险
-        eval(value as string);
-
-        // 恢复原始的 console.log 和 console.error
-        console.log = oldLog;
-        console.error = oldError;
-      } catch (error: any) { // 使用 any 类型来捕获错误，实际项目中应避免使用 any
-        appendMessage(`Error: ${error.message}`, true);
+      setJs(value || '');
+  
+      // 清空控制台
+      appendMessage('', false); // 这里可以传入一个空字符串和false来清空控制台
+  
+      if (value && value.trim()) { // 只有在代码非空的情况下才执行eval
+        try {
+          const oldLog = console.log;
+          const oldError = console.error;
+  
+          // 重定向 console.log 和 console.error
+          console.log = (message: any) => {
+            appendMessage(`${message}`, false);
+          };
+          console.error = (error: any) => {
+            appendMessage(`Error: ${error}`, true);
+          };
+  
+          // 注意：使用 eval() 有一定的安全风险
+          eval(value as string);
+  
+          // 恢复原始的 console.log 和 console.error
+          console.log = oldLog;
+          console.error = oldError;
+        } catch (error: any) {
+          appendMessage(`Error: ${error.message}`, true);
+        }
       }
     };
   };
@@ -61,17 +67,17 @@ const Home: React.FC = () => {
 
   return (
     <div style={{padding:'10px'}}>
-      <div style={{width:'100vw',height:'30px',backgroundColor:'green',textAlign:'center',lineHeight:'30px',fontSize:'25px',color:'white',letterSpacing:'20px'}}>PlayCode.io</div>
+      <div style={{width:'100vw',height:'30px',backgroundColor:'#5EC2B9',textAlign:'center',lineHeight:'30px',fontSize:'25px',color:'white',letterSpacing:'20px'}}>PlayCode.io</div>
     <div style={{width:'100vw',height:'100vh',display:'flex',flexDirection:'column',marginTop:'5px'}}>
       
       <div style={{display:'flex',flexDirection:'row',height:'20vh'}}>
 
       <Resizable
-            style={{background: "#8cdbd5",display:'flex',flexDirection:'column'}}
+            style={{background: "#8cdbd5",display:'flex',flexDirection:'column',boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px',marginRight:'10px'}}
             defaultSize={{width:'50vw',height:'30vh'}}
             >
-            <div style={{height:'15px',fontSize:'10px',marginLeft:'10px'}}>HTML</div>
-          <div style={{width:'100%',height:'100%'}}>
+            <div style={{height:'15px',fontSize:'20px',marginLeft:'10px',zIndex:2}}>HTML</div>
+          <div style={{width:'100%',height:'100%',marginTop:'15px'}}>
             <CodeEditor language="html" value={html} onChange={(value) => handleEditorChange('html', value)} />
           </div>
           </Resizable>
@@ -86,32 +92,32 @@ const Home: React.FC = () => {
           </div>
           </Resizable> */}
       <Resizable
-            style={{background: "#8cdbd5",display:'flex',flexDirection:'column',marginLeft:'20px'}}
+            style={{background: "#8cdbd5",display:'flex',flexDirection:'column',marginLeft:'20px',boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px'}}
             defaultSize={{width:'50vw',height:'30vh'}}
             >
-            <div style={{height:'15px',fontSize:'10px',marginLeft:'10px',width:'100%'}}>CONSOLE</div>
-          <div id="console" style={{ width:'100%',height:'100%',padding: '10px', backgroundColor:'white', color: 'red' }}>
+            <div style={{height:'15px',fontSize:'20px',marginLeft:'10px',width:'100%',zIndex:2}}>CONSOLE</div>
+          <div id="console" style={{ width:'100%',height:'100%',padding: '10px', backgroundColor:'white', color: 'red' ,marginTop:'15px'}}>
           </div>
           </Resizable>
       </div>
 
       <div style={{display:'flex',flexDirection:'row',marginTop:'130px',height:'50vh',}}>
       <Resizable
-            style={{background: "#8cdbd5",display:'flex',flexDirection:'column'}}
+            style={{background: "#8cdbd5",display:'flex',flexDirection:'column',boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px',marginRight:'10px'}}
             defaultSize={{width:'50vw',height:'30vh'}}
             >
-            <div style={{height:'15px',fontSize:'10px',marginLeft:'10px',width:'100%'}}>javascript</div>
-          <div style={{width:'100%',height:'100%'}}>
+            <div style={{height:'15px',fontSize:'20px',marginLeft:'10px',width:'100%',zIndex:2}}>javascript</div>
+          <div style={{width:'100%',height:'100%',marginTop:'15px'}}>
             <CodeEditor language="javascript" value={js} onChange={(value) => handleEditorChange('javascript', value)} />
           </div>
           </Resizable>
 
           <Resizable
-            style={{display:'flex',flexDirection:'column'}}
+            style={{display:'flex',flexDirection:'column',boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px'}}
             defaultSize={{width:'50vw',height:'50vh'}}
             >
-            <div style={{background: "#8cdbd5",height:'15px',fontSize:'10px',marginLeft:'10px',width:'100%'}}>javascript</div>
-          <div style={{width:'100%',height:'100%'}}>
+            <div style={{background: "#8cdbd5",height:'30px',fontSize:'20px',marginLeft:'10px',width:'100%'}}>Web View</div>
+          <div style={{width:'100%',height:'100%',marginTop:'15px'}}>
             <WebView html={html} css={css} js={js} />
           </div>
           </Resizable>
